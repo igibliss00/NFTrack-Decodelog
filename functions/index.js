b404eccda0cced2ff29f79174d42d36de96a0626
igibliss00@gmail.com <igibliss00@gmail.com>
2021-06-26 16:38:39 -0400

    Update Firestore with the token

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,41 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
 const Web3 = require("web3");
 const purchaseABI = require("./contracts/PurchaseABI.json");
 require('dotenv').config();
 const infuraKeys = process.env.INFURA_API_KEY || ""
 const web3 = new Web3(`wss://rinkeby.infura.io/ws/v3/${infuraKeys}`);
 
+admin.initializeApp();
+
 exports.decodeLog = functions.https.onRequest( async (request, response) => {
-    const { hexString, topics } = request.body
+    const { hexString, topics, documentID } = request.body
     let decoded = web3.eth.abi.decodeLog(
       inputs,
       hexString,
       topics
     )
 
-    response.send(decoded)
+    console.log("decoded", decoded)
+    console.log("tokenId", decoded.tokenId)
+    const postRef = admin.firestore()
+      .collection("post")
+      .doc(documentID)
+
+    postRef.get().then((doc) => {
+      if(doc.exists) {
+        postRef.update({ tokenId: decoded.tokenId })
+        .catch(err => {
+          console.log("Error", err)
+          response.send("500")
+        })
+      }
+    }).catch(err => {
+      console.log("Internal server error", err)
+      response.send("500")
+    })
+
+    response.send("200")
 });
 
 const inputs = [
